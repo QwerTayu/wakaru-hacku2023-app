2dc8af3c09e910fa12f0c198b3acbb4d40b95f26
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-15 09:57:48 +0900

    ♻️ ページ内をコンポーネント化

---
+++ src/components/StatusContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Content.module.css'
+
+function StatusContent() {
+  return (
+    <div className={styles.container}>
+    status
+    </div>
+  )
+}
+
+export default StatusContent;
