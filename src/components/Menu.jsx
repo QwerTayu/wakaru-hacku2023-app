3bf0a6a924b7b65929e312c59a59d501ff7ff7ee
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-15 16:05:30 +0900

    ✨ #17 ホームページの追加

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,12 +4,15 @@ import styles from "./Menu.module.css";
 function Menu() {
   return (
     <div className={styles.menu}>
-        <Link href="map">
-            Map
-        </Link>
-        <Link href="status">
-            Status
-        </Link>
+      <Link href="/">
+        Home
+      </Link>
+      <Link href="status">
+        Status
+      </Link>
+      <Link href="map">
+        Map
+      </Link>
     </div>
   );
 }
