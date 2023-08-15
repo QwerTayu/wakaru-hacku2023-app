4e9cff1d66cc409a126dacad298d3509c9c330be
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-15 17:17:20 +0900

    ♻️ firebaseフレームワークを複数使えるようにオブジェクト化しました

--- src/components/PlaceInfo.jsx
+++ src/components/PlaceInfo.jsx
@@ -1,20 +1,16 @@
 import { InfoWindowF, MarkerF } from "@react-google-maps/api";
 import React, { useEffect, useState } from "react";
 import styles from "./Map.module.css";
-import db from "@/firebase";
+import firebase from "@/firebase";
 import { collection, getDocs, onSnapshot } from "firebase/firestore";
 
 export default function PlaceInfo () {
-    const places = [
-        { info: "info1", location: { lat: 35.048225, lng: 139.49701 } },
-        { info: "info2", location: { lat: 36.048225, lng: 140.49701 } },
-    ];
 
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
         // データベースからデータを取得する
-        const userData = collection(db, "user");
+        const userData = collection(firebase.db, "user");
         getDocs(userData).then((snapShot) => {
             // console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
             setUsers(snapShot.docs.map((doc) => ({ ...doc.data() })));
