8609f406cdc63bb270a9bf1725962a4c342124a1
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-16 11:59:40 +0900

    ✨ Home, Statusの中身を追加

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './Content.module.css'
+import UserInfo from "@/components/UserInfo";
 
 function HomeContent() {
+
+  const [userStatus, setUserStatus] = useState(false)
+
   return (
     <div className={styles.container}>
-    home
+    <UserInfo />
+    <div className={styles.userStatus}>
+      {userStatus ? `出勤中` : `退勤中`}
+      <button
+      onClick={(e) => setUserStatus(!userStatus)}
+    >
+      {!userStatus ? `出勤` : `退勤`}
+    </button>
+    <h3>退勤予定時刻</h3>
+    <p className={styles.goHomeTime}>12:00:00</p>
+    </div>
     </div>
   )
 }
--- src/components/StatusContent.jsx
+++ src/components/StatusContent.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import { useState } from 'react';
 import styles from './Content.module.css'
 
 function StatusContent() {
+  const members = [
+    {
+      name: '山田太郎',
+      icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    },
+    {
+      name: '山田次郎',
+      icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    },
+    {
+      name: '山田三郎',
+      icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    },
+    {
+      name: '山田四郎',
+      icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    },
+    {
+      name: '山田五郎',
+      icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    }
+  ];
+
   return (
     <div className={styles.container}>
-    status
+      <p className={styles.organizaion}>明石高専</p>
+      <div className={styles.members}>
+        {members.map((member) => (
+          <div key={member.name} className={styles.member}>
+            <img src={member.icon} alt="" />
+            <p>{member.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
