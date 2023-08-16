da89e46af4f5a5734ebcdad88cf03a1dca9ed1c8
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-17 01:22:22 +0900

    💄 #33 フォントの読み込みをここで行います。<link>を追加 @e2137

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link href='https://fonts.googleapis.com/css?family=Krona One' rel="stylesheet" />
+      <link href='https://fonts.googleapis.com/css?family=Gravitas One' rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
